11dbca1c99e2b2f727d5b38dd0d16da44e9ad102
Herbert Hipolito <hipolito160@gmail.com>
2022-01-07 12:54:53 -0300

    extract and speak

---
+++ english.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import pytesseract
+import pyttsx3 
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'      
+
+class SpeakingText():
+
+    def __init__(self,string,language,rate_word=200,show=True):
+
+        self.string=string
+        self.letter_list= [letter for letter in string]
+        self.show=show
+        self.language=language
+        self.rate=rate_word
+
+    def removing_all_symbols(self):
+
+        for index in range(len(self.symbols)):
+            self.remove_one_symbol(self.symbols[index])    
+
+    def remove_one_symbol(self,symbol):
+        
+        #Receive a list. Remove the desired characters in the same order in the 'symbol'.
+        
+        condition = True
+        
+        for index in range(len(self.letter_list)-len(symbol)):
+
+            for index2 in range(len(symbol)):
+                condition = self.letter_list[index+index2] == symbol[index2]
+                if not condition:
+                    break
+            
+            if condition:
+                for j in range(len(symbol)):
+                    self.letter_list.pop(index)
+                self.remove_one_symbol(symbol)
+                
+                break
+
+    def set_symbols(self,symbols):
+        self.symbols=symbols
+
+    def set_rate_word(self,rate):
+        self.rate=rate
+        
+    def get_string(self):
+        letters=""
+        for letter in self.letter_list:
+            letters+=letter
+        return letters
+
+    def SpeakText(self,command):
+        #Check if the language chosen to speak was installed properly and speak the received text. 
+        engine = pyttsx3.init()
+        number= -1
+        
+        for index,voice in enumerate(engine.getProperty('voices')):
+            if self.language in voice.name:
+                number = index
+        if number == -1:
+            print('The language was not recognized, check if your system has it installed in the language settings.')
+            return None
+        
+        engine.setProperty("voice", engine.getProperty('voices')[number].id)
+        engine.setProperty('rate', self.rate)
+        engine.say(command)
+        engine.runAndWait()
+    
+
+text=SpeakingText(pytesseract.image_to_string(r'C:\Users\Usuario\Desktop\projetos paralelos\image_english\text2.png'),'Spanish',140)
+to_remove = [['(',str(number),')'] for number in range(0,10)]
+to_remove2 = [[str(number),' '] for number in range(0,10)]
+to_remove3 = ['{']
+
+
+text.set_symbols([*to_remove,*to_remove2,*to_remove3])
+text.removing_all_symbols()
+#text.set_symbols(to_remove2)
+#text.removing_all_symbols()
+#text.set_symbols(to_remove3)
+#text.removing_all_symbols()
+print(text.get_string())
+text.SpeakText(text.get_string())
